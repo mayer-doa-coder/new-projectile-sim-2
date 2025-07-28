bc1696d52d7866ff4cb0b02f09fc7df085723bf2
Arka Nath <natharka55@gmail.com>
2025-07-28 23:42:20 +0600

    basic demo

---
+++ projectile-sim/src/app/page.tsx
@@ -0,0 +1,9 @@
+import ProjectileSimulator from '@/components/ProjectileSimulator';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gradient-to-br from-blue-100 to-green-100 py-8">
+      <ProjectileSimulator />
+    </main>
+  );
+}
